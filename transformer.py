7e5ba448a265858e3e67499e09b3518624f09f9b
Andres Gonzalez <anfego22@gmail.com>
2024-03-13 09:51:26 -0500

    Add decision transformer

--- transformer.py
+++ transformer.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from typing import Dict
+from torch.distributions import Categorical
 
 class Head(nn.Module):
     def __init__(self, n_embd: int, head_size: int, context: int, mask: bool = True, dropout_p: float = .1):
@@ -62,3 +64,47 @@ class Transformer(nn.Module):
         x = self.out_embd(x) + self.pos_embd(self.pos[:T])
         x = self.block(x)
         return self.lin(self.layN(x))
+
+
+class DecisionTransformer(nn.Module):
+    def __init__(self, action_size: int, obs_size: int, n_embd: int, context: int, 
+                 n_heads: int = 4, n_blocks: int = 8,
+                 mask: bool = True, max_length: int = 1024, dropout_p: float = .1):
+        super().__init__()
+        self.context = context
+        self.pos_embd = nn.Embedding(max_length, n_embd)
+        self.ae = nn.Embedding(action_size, n_embd)
+        self.se = nn.Linear(obs_size, n_embd)
+        self.re = nn.Linear(1, n_embd)
+        self.block = nn.Sequential(*[Block(n_embd, context * 3, n_heads, mask, dropout_p) for _ in range(n_blocks)])
+        self.layn = nn.LayerNorm(n_embd)
+        self.linS = nn.Linear(n_embd, obs_size)
+        self.linA = nn.Linear(n_embd, action_size)
+        self.linR = nn.Linear(n_embd, 1)
+
+    def forward(self, x: Dict[str, torch.tensor]):
+        '''Forward pass.
+        
+        Input a dictionary with keys R, s, a
+        x['R']: Returns to go shape B, T, 1
+        x['s']: States shape B, T, obs_size
+        x['a']: Actions shape B, T (in discrete case)
+        x['t']: Timestam shape B, T
+        '''
+        B, T, _ = x['s'].shape
+        T = min(self.context, T)
+        ae = self.ae(x['a'][:, -T:]) + self.pos_embd(x['t'][:, -T:])
+        se = self.se(x['s'][:, -T:, :]) + self.pos_embd(x['t'][:, -T:])
+        re = self.re(x['R'][:, -T:, :]) + self.pos_embd(x['t'][:, -T:])
+        x = torch.stack([re, se, ae], dim=1).permute(0, 2, 1, 3).reshape(B, T * 3, -1)
+        x = self.block(x)
+        x = self.layn(x)
+        # Copy from https://github.com/kzl/decision-transformer/blob/master/gym/decision_transformer/models/decision_transformer.py
+        x = x.reshape(B, T, 3, -1).permute(0, 2, 1, 3)
+        return self.linR(x[:, 2]), self.linS(x[:, 2]), self.linA(x[:, 1])
+        
+    def act(self, x: Dict[str, torch.tensor]):
+        _, _, logits = self(x)
+        a_dist = Categorical(logits = logits[:, -1])
+        return a_dist.sample().item()
+        
